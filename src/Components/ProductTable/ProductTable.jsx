a1a2035d560dcea659a056fd71c712ab4df6fc7c
masome hosine <hosine.masome@gmail.com>
2024-01-08 13:29:31 +0330

    create table for Creating a table to display productsگ

---
+++ src/Components/ProductTable/ProductTable.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import './ProductTable.css'
+import DeleteModal from '../DeleteModal/DeleteModal'
+import DetailsModal from '../DetailsModal/DetailsModal'
+
+function ProductTable() {
+  const [isShowModal, setIsShowModal] = useState(false)
+  const [isShowDetailsModal, setIsShowDetailsModal] = useState(false)
+  const deleteModalCancelAction = () => {
+    console.log('مدال کنسل شد')
+    setIsShowModal(false)
+  }
+  const deleteModalSubmitAction = () => {
+    console.log('مدال تایید شد')
+    setIsShowModal(false)
+  }
+
+  const closeDetailsModal = () => {
+    console.log('مودال بسته شد')
+    setIsShowDetailsModal(false)
+  }
+  return (
+    <>
+      <div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">عکس</th>
+              <th scope="col">اسم</th>
+              <th scope="col">قیمت</th>
+              <th scope="col">موجودی</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="">
+                <img
+                  src="/images/oil.jpeg"
+                  className="img-fluid img-table"
+                  alt="oil"
+                />
+              </th>
+              <td>
+                <p> روغن سرخ کردنی</p>
+              </td>
+              <td>92000تومان</td>
+              <td>82</td>
+              <td>
+                <button
+                  className="products-table-btn btn"
+                  onClick={() => setIsShowDetailsModal(true)}
+                >
+                  جزئیات
+                </button>
+                <button
+                  className="products-table-btn btn mx-2"
+                  onClick={() => setIsShowModal(true)}
+                >
+                  حذف{' '}
+                </button>
+                <button className="products-table-btn btn"> ویرایش</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {isShowModal && (
+        <DeleteModal
+          submitAction={deleteModalSubmitAction}
+          cancelAction={deleteModalCancelAction}
+        />
+      )}
+      {isShowDetailsModal && <DetailsModal onHide={closeDetailsModal} />}
+    </>
+  )
+}
+
+export default ProductTable

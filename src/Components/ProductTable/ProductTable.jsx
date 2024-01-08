a1a7d037ca0a5334fec0dbd52f34f1de47b6cf68
masome hosine <hosine.masome@gmail.com>
2024-01-08 19:05:07 +0330

    Getting data from the backend and displaying product data and adding components ErrorBox

--- src/Components/ProductTable/ProductTable.jsx
+++ src/Components/ProductTable/ProductTable.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './ProductTable.css'
 import DeleteModal from '../DeleteModal/DeleteModal'
 import DetailsModal from '../DetailsModal/DetailsModal'
 import EditModal from '../EditModal/EditModal'
+import { AiFillDollarCircle } from 'react-icons/ai'
+import Errorbox from '../Errorbox/Errorbox'
 
 function ProductTable() {
   const [isShowModal, setIsShowModal] = useState(false)
   const [isShowDetailsModal, setIsShowDetailsModal] = useState(false)
   const [isShowEditModal, setIsShowEditModal] = useState(false)
+  const [allProduct, setAllProduct] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/products')
+      .then((res) => res.json())
+      .then((products) => setAllProduct(products))
+  }, [])
 
   const deleteModalCancelAction = () => {
     console.log('مدال کنسل شد')
@@ -28,56 +37,65 @@ function ProductTable() {
   }
   return (
     <>
-      <div>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">عکس</th>
-              <th scope="col">اسم</th>
-              <th scope="col">قیمت</th>
-              <th scope="col">موجودی</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="">
-                <img
-                  src="/images/oil.jpeg"
-                  className="img-fluid img-table"
-                  alt="oil"
-                />
-              </th>
-              <td>
-                <p> روغن سرخ کردنی</p>
-              </td>
-              <td>92000تومان</td>
-              <td>82</td>
-              <td>
-                <button
-                  className="products-table-btn btn"
-                  onClick={() => setIsShowDetailsModal(true)}
-                >
-                  جزئیات
-                </button>
-                <button
-                  className="products-table-btn btn mx-2"
-                  onClick={() => setIsShowModal(true)}
-                >
-                  حذف{' '}
-                </button>
-                <button
-                  className="products-table-btn btn"
-                  onClick={() => setIsShowEditModal(true)}
-                >
-                  {' '}
-                  ویرایش
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {allProduct.length ? (
+        <div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">عکس</th>
+                <th scope="col">اسم</th>
+                <th scope="col">قیمت</th>
+                <th scope="col">موجودی</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {allProduct.map((product) => {
+                return (
+                  <tr key={product.id}>
+                    <th scope="">
+                      <img
+                        src={product.img}
+                        className="img-fluid img-table"
+                        alt="oil"
+                      />
+                    </th>
+                    <td>
+                      <p> {product.title}</p>
+                    </td>
+                    <td>{product.price} تومان</td>
+                    <td>{product.count}</td>
+                    <td>
+                      <button
+                        className="products-table-btn btn"
+                        onClick={() => setIsShowDetailsModal(true)}
+                      >
+                        جزئیات
+                      </button>
+                      <button
+                        className="products-table-btn btn mx-2"
+                        onClick={() => setIsShowModal(true)}
+                      >
+                        حذف{' '}
+                      </button>
+                      <button
+                        className="products-table-btn btn"
+                        onClick={() => setIsShowEditModal(true)}
+                      >
+                        {' '}
+                        ویرایش
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <Errorbox msg={'هیچ محصولی یافت نشد'} />
+      )}
+
       {isShowModal && (
         <DeleteModal
           submitAction={deleteModalSubmitAction}
@@ -89,7 +107,58 @@ function ProductTable() {
         <EditModal
           onClose={() => setIsShowEditModal(false)}
           onSubmit={(event) => updateProductInfos(event)}
-        />
+        >
+          <div className="edit-product-form-group">
+            <span>
+              <AiFillDollarCircle />
+            </span>
+            <input
+              type="text "
+              className="edit-product-input"
+              placeholder="عنوان جدیدرا  وارد کنید"
+            />
+          </div>
+          <div className="edit-product-form-group">
+            <span>
+              <AiFillDollarCircle />
+            </span>
+            <input
+              type="text "
+              className="edit-product-input"
+              placeholder="عنوان جدیدرا  وارد کنید"
+            />
+          </div>{' '}
+          <div className="edit-product-form-group">
+            <span>
+              <AiFillDollarCircle />
+            </span>
+            <input
+              type="text "
+              className="edit-product-input"
+              placeholder="عنوان جدیدرا  وارد کنید"
+            />
+          </div>{' '}
+          <div className="edit-product-form-group">
+            <span>
+              <AiFillDollarCircle />
+            </span>
+            <input
+              type="text "
+              className="edit-product-input"
+              placeholder="عنوان جدیدرا  وارد کنید"
+            />
+          </div>{' '}
+          <div className="edit-product-form-group">
+            <span>
+              <AiFillDollarCircle />
+            </span>
+            <input
+              type="text "
+              className="edit-product-input"
+              placeholder="عنوان جدیدرا  وارد کنید"
+            />
+          </div>
+        </EditModal>
       )}
     </>
   )

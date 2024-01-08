3bb3c77c324a6c94977ae64723c9dfc86d3e5a20
masome hosine <hosine.masome@gmail.com>
2024-01-08 13:48:54 +0330

    comment page

---
+++ src/Components/Comments/Comments.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Errorbox from '../Errorbox/Errorbox'
+
+function Comments() {
+  return (
+    <>
+      <Errorbox msg={'هیچ کامنتی یافت نشد'} />
+    </>
+  )
+}
+
+export default Comments

8ec6ff25128a0b580e42ad05a8105406dc5058dc
masome hosine <hosine.masome@gmail.com>
2024-01-08 19:05:33 +0330

    remove Errorbox componet

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Errorbox from '../Errorbox/Errorbox'
 import AddNewProduct from '../AddNewProduct/AddNewProduct'
 import ProductTable from '../ProductTable/ProductTable'
 function Products() {
   return (
     <>
-      <Errorbox msg={'هیچ محصولی یافت نشد'} />
       <AddNewProduct />
       <ProductTable />
     </>

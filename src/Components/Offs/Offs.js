2c45d5234d9f8c5bfa98868c16dcdf61094ecc51
masome hosine <hosine.masome@gmail.com>
2024-01-08 13:50:54 +0330

    offs page

---
+++ src/Components/Offs/Offs.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Errorbox from '../Errorbox/Errorbox'
+
+function Offs() {
+  return <Errorbox msg={'هیچ سفارشی به عنوان پیغام یافت نشد'} />
+}
+
+export default Offs

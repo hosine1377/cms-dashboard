f30ae4709f48cea00520d0588e1c0a29db73938d
masome hosine <hosine.masome@gmail.com>
2024-01-17 23:46:14 +0330

    add new products feature

--- src/Components/AddNewProduct/AddNewProduct.jsx
+++ src/Components/AddNewProduct/AddNewProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddNewProduct.css'
 //import icon component from react icon
 import { FiDollarSign } from 'react-icons/fi'
@@ -7,6 +7,40 @@ import { MdAddPhotoAlternate } from 'react-icons/md'
 import { MdColorLens } from 'react-icons/md'
 import { MdOutlineDriveFileRenameOutline } from 'react-icons/md'
 function AddNewProduct() {
+  const [newProductTitle, setNewProductTilte] = useState('')
+  const [newProductPrice, setNewProductPrice] = useState('')
+  const [newProductCount, setNewProductCount] = useState('')
+  const [newProductimg, setNewProductimg] = useState('')
+  const [newProductPopularity, setNewProductPopularity] = useState('')
+  const [newProductSale, setNewProductSale] = useState('')
+  const [newProductColors, setNewProductColors] = useState('')
+
+  const newProductsInfos = {
+    title: newProductTitle,
+    price: newProductPrice,
+    count: newProductCount,
+    img: newProductimg,
+    popularity: newProductPopularity,
+    sale: newProductSale,
+    colors: newProductColors,
+  }
+  console.log(newProductsInfos)
+  const addNewProduct = (event) => {
+    event.preventDefault()
+    console.log('محصول اضافه شد')
+    fetch(`http://localhost:3000/api/products`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newProductsInfos),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result)
+      })
+  }
+
   return (
     <div className="addNewProduct">
       <h1 className="title">افزودن محصول جدید</h1>
@@ -15,13 +49,23 @@ function AddNewProduct() {
           <div className="col-6">
             <div className="form-group">
               <MdOutlineDriveFileRenameOutline />
-              <input type="text" placeholder="اسم محصول را بنویسید" />
+              <input
+                value={newProductTitle}
+                onChange={(event) => setNewProductTilte(event.target.value)}
+                type="text"
+                placeholder="اسم محصول را بنویسید"
+              />
             </div>
           </div>
           <div className="col-6">
             <div className="form-group">
               <FiDollarSign />
-              <input type="text" placeholder="قیمت محصول را بنویسید" />
+              <input
+                value={newProductPrice}
+                onChange={(event) => setNewProductPrice(event.target.value)}
+                type="text"
+                placeholder="قیمت محصول را بنویسید"
+              />
             </div>
           </div>
         </div>
@@ -29,13 +73,23 @@ function AddNewProduct() {
           <div className="col-6">
             <div className="form-group">
               <AiOutlineShopping />
-              <input type="text" placeholder="موجودی محصول را بنویسید" />
+              <input
+                value={newProductCount}
+                onChange={(event) => setNewProductCount(event.target.value)}
+                type="text"
+                placeholder="موجودی محصول را بنویسید"
+              />
             </div>
           </div>
           <div className="col-6">
             <div className="form-group">
               <FiDollarSign />
-              <input type="text" placeholder="ادرس عکس محصول را بنویسید" />
+              <input
+                value={newProductimg}
+                onChange={(event) => setNewProductimg(event.target.value)}
+                type="text"
+                placeholder="ادرس عکس محصول را بنویسید"
+              />
             </div>
           </div>
         </div>
@@ -44,6 +98,10 @@ function AddNewProduct() {
             <div className="form-group">
               <MdAddPhotoAlternate />
               <input
+                value={newProductPopularity}
+                onChange={(event) =>
+                  setNewProductPopularity(event.target.value)
+                }
                 type="text"
                 placeholder="میزان محبوبیت  محصول را بنویسید"
               />
@@ -52,7 +110,12 @@ function AddNewProduct() {
           <div className="col-6">
             <div className="form-group">
               <FiDollarSign />
-              <input type="text" placeholder="میزان فروش محصول را بنویسید" />
+              <input
+                value={newProductSale}
+                onChange={(event) => setNewProductSale(event.target.value)}
+                type="text"
+                placeholder="میزان فروش محصول را بنویسید"
+              />
             </div>
           </div>
         </div>
@@ -61,13 +124,20 @@ function AddNewProduct() {
             <div className="form-group">
               <MdColorLens />
               <input
+                value={newProductColors}
+                onChange={(event) => setNewProductColors(event.target.value)}
                 type="text"
                 placeholder="تعداد  رنگ بندی  محصول را بنویسید"
               />
             </div>
           </div>
         </div>
-        <button className="addNewProduct-btn btn ">ثبت محصول</button>
+        <button
+          className="addNewProduct-btn btn "
+          onClick={(event) => addNewProduct(event)}
+        >
+          ثبت محصول
+        </button>
       </form>
     </div>
   )
